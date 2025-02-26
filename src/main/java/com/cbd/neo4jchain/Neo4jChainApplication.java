7192ae8ed94bc50ad428eaa3aeccdd616c33a9b6
luchersol <lmherencia2003@gmail.com>
2025-02-26 20:54:22 +0100

    Init commit

---
+++ src/main/java/com/cbd/neo4jchain/Neo4jChainApplication.java
@@ -0,0 +1,13 @@
+package com.cbd.neo4jchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbd/neo4jchain/Neo4jChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbd.neo4jchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
