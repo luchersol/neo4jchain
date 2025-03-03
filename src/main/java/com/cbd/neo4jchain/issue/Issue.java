ac6f2cbce3f00a63b8ee4f3af020f547d0a850fd
luchersol <lmherencia2003@gmail.com>
2025-03-03 20:32:22 +0100

    feat: add new relationship

--- src/main/java/com/cbd/neo4jchain/issue/Issue.java
+++ src/main/java/com/cbd/neo4jchain/issue/Issue.java
@@ -3,8 +3,10 @@ package com.cbd.neo4jchain.issue;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
 
+import com.cbd.neo4jchain.person.Person;
 import com.cbd.neo4jchain.service_org.ServiceOrg;
 import com.cbd.neo4jchain.status.Status;
+import com.cbd.neo4jchain.team.Team;
 
 import lombok.Getter;
 
@@ -21,4 +23,12 @@ public class Issue {
     @Relationship(type = "STATUS")
     private Status status;
 
+    @Relationship(type = "ASIGNED_TO")
+    private Team assignedTeam;
+
+    @Relationship(type = "ASIGNED_TO")
+    private Person assignedPerson;
+
+    @Relationship(type = "CREATED_BY")
+    private Person owner;
 }
