89c6fb8069d731954ed85a06568753a5107f13a4
luchersol <lmherencia2003@gmail.com>
2025-03-22 18:53:18 +0100

    fix: solve cors problem

---
+++ src/main/java/com/cbd/neo4jchain/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.cbd.neo4jchain.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Permitir el frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // ✅ Permitir todos los headers, incluyendo Authorization
+                        .allowCredentials(true); // Permitir autenticación con cookies o headers
+            }
+        };
+    }
+}
--- src/main/java/com/cbd/neo4jchain/configuration/SecurityConfiguration.java
+++ src/main/java/com/cbd/neo4jchain/configuration/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.cbd.neo4jchain.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,18 +9,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
-    
-    @Bean
+
+	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		
+
 		http
-			.cors(withDefaults())		
-			.csrf(AbstractHttpConfigurer::disable)		
-			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))			
-			.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable()))			
-			.authorizeHttpRequests(authorizeRequests ->	authorizeRequests
-			.anyRequest().permitAll());	
+				.csrf(AbstractHttpConfigurer::disable)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable()))
+				.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+						.anyRequest().permitAll());
 		return http.build();
 	}
-    
+
 }
