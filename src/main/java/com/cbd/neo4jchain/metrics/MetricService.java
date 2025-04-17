bb39aa6f61ab7b7eceb7e6f60476d834468f9d4e
luchersol <lmherencia2003@gmail.com>
2025-04-18 00:05:33 +0200

    fix: fix problem with logic in metrcis

--- src/main/java/com/cbd/neo4jchain/metrics/MetricService.java
+++ src/main/java/com/cbd/neo4jchain/metrics/MetricService.java
@@ -10,6 +10,7 @@ import java.util.function.Predicate;
 import org.springframework.stereotype.Service;
 
 import com.cbd.neo4jchain.customer.Customer;
+import com.cbd.neo4jchain.enums.Metric;
 import com.cbd.neo4jchain.issue.Issue;
 import com.cbd.neo4jchain.issue.IssueRepository;
 import com.cbd.neo4jchain.objective.Objective;
@@ -33,45 +34,48 @@ public class MetricService {
         this.organizationRepository = organizationRepository;
     }
 
-    private static BiPredicate<Objective, Issue> PASS_TTO = (obj, i) -> obj.toSecond() >= i.getTTO();
-    private static BiPredicate<Objective, Issue> PASS_TTR = (obj, i) -> obj.toSecond() >= i.getTTR();
-    private static BiPredicate<Objective, Issue> PASS_SLA = (obj, i) -> PASS_TTO.test(obj, i) && PASS_TTR.test(obj, i);
+    private static BiPredicate<Objective, Issue> PASS_TTO = (obj, i) -> obj != null && obj.toSecond() >= i.getTTO();
+    private static BiPredicate<Objective, Issue> PASS_TTR = (obj, i) -> obj != null && obj.toSecond() >= i.getTTR();
+
+    private enum CheckMetric {
+        CHECK_TTO, CHECK_TTR, CHECK_SLA, CHECK_NOT_SLA
+    }
 
     public Double getGlobalPassedSLA(Long chainId) {
         List<Issue> issues = getIssues(chainId, issue -> issue.getStatus().isTerminal());
-        return processIssues(issues, PASS_SLA);
+        return processIssues(issues, CheckMetric.CHECK_SLA);
     }
 
     public Double getProblematicOpenIssues(Long chainId) {
         List<Issue> issues = getIssues(chainId, issue -> !issue.getStatus().isTerminal());
-        return processIssues(issues, PASS_SLA.negate());
+        return processIssues(issues, CheckMetric.CHECK_NOT_SLA);
     }
 
     public Double getPassedTTO(Long chainId) {
         List<Issue> issues = getIssues(chainId, _ -> true);
-        return processIssues(issues, PASS_TTO);
+        return processIssues(issues, CheckMetric.CHECK_TTO);
     }
 
     public Double getPassedTTR(Long chainId) {
         List<Issue> issues = getIssues(chainId, _ -> true);
-        return processIssues(issues, PASS_TTR);
+        return processIssues(issues, CheckMetric.CHECK_TTR);
     }
 
     public Double getMonthlyPassedSLA(Long chainId) {
         List<Issue> issues = getIssues(chainId,
                 issue -> issue.getCreatedAt().getMonth().equals(LocalDate.now().getMonth())
                         && issue.getStatus().isTerminal());
-        return processIssues(issues, PASS_SLA);
+        return processIssues(issues, CheckMetric.CHECK_SLA);
     }
 
     public Double getServicePassedTTO(Long serviceId) {
         List<Issue> issues = issueRepository.findByServiceOrg_Id(serviceId);
-        return processIssues(issues, PASS_TTO);
+        return processIssues(issues, CheckMetric.CHECK_TTO);
     }
 
     public Double getServicePassedTTR(Long serviceId) {
         List<Issue> issues = issueRepository.findByServiceOrg_Id(serviceId);
-        return processIssues(issues, PASS_TTR);
+        return processIssues(issues, CheckMetric.CHECK_TTR);
     }
 
     private List<Issue> getIssues(Long chainId, Predicate<Issue> predicate) {
@@ -89,9 +93,9 @@ public class MetricService {
         return issues;
     }
 
-    private Double processIssues(List<Issue> issues, BiPredicate<Objective, Issue> predicate) {
+    private Double processIssues(List<Issue> issues, CheckMetric checkMetric) {
         Integer cont = 0;
-        issuesBucle: for (Issue issue : issues) {
+        for (Issue issue : issues) {
             ServiceOrg serviceOrg = issue.getServiceOrg();
             if (serviceOrg == null)
                 continue;
@@ -113,10 +117,26 @@ public class MetricService {
                                 if (scope.getRequestType() != null &&
                                         Objects.equals(scope.getRequestType(), issue.getRequestType())) {
                                     List<Objective> objectives = scope.getObjectives();
-                                    for (Objective objective : objectives) {
-                                        if (predicate.test(objective, issue)) {
-                                            cont++;
-                                            continue issuesBucle;
+                                    Objective objTTO = objectives.stream().filter(obj -> obj.getMetric() == Metric.TTO)
+                                            .findFirst().orElse(null);
+                                    Objective objTTR = objectives.stream().filter(obj -> obj.getMetric() == Metric.TTR)
+                                            .findFirst().orElse(null);
+                                    switch (checkMetric) {
+                                        case CHECK_NOT_SLA -> {
+                                            if (!(PASS_TTO.test(objTTO, issue) && PASS_TTR.test(objTTR, issue)))
+                                                cont++;
+                                        }
+                                        case CHECK_SLA -> {
+                                            if (PASS_TTO.test(objTTO, issue) && PASS_TTR.test(objTTR, issue))
+                                                cont++;
+                                        }
+                                        case CHECK_TTO -> {
+                                            if (PASS_TTO.test(objTTO, issue))
+                                                cont++;
+                                        }
+                                        case CHECK_TTR -> {
+                                            if (PASS_TTR.test(objTTR, issue))
+                                                cont++;
                                         }
                                     }
                                 }
